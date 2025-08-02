7f35fde0b1dc3d6693359306e563feccff068690
Aleksandar Sapic <sapicalleksandar@gmail.com>
2025-08-02 13:17:38 +0200

    Initial commit

---
+++ backend/src/test/java/com/aleksandar/streaming_platform/backend/StreamingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandar.streaming_platform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
